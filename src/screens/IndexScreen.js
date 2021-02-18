e299ecfb80c657c349be99cda9f27e5c25068cba
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-02-18 11:13:17 +0700

    initial setup and add IndexScreen.js

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Text>This is a second line</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default IndexScreen;
