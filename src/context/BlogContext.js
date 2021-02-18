0c08670c4a3d3e8c84779590b00305b5800df0be
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-02-18 17:03:56 +0700

    add addBlogPost to BlogProvider.js

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,14 +3,14 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [
-    { title: 'Blog Post #1' },
-    { title: 'Blog Post #2' },
-    { title: 'Blog Post #3' }
-  ];
+  const [ blogPosts, setBlogPosts ] = React.useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([ ...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`} ])
+  };
 
   return (
-    <BlogContext.Provider value={blogPosts}>
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
       {children}
     </BlogContext.Provider>
   );
