a8dcb84d0261d42cf009df5fc8ac69ea14ebb1ab
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-02-19 14:39:14 +0700

    useContext in ShowScreen.js

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context as BlogContext } from '../context/BlogContext';
 
-const ShowScreen = () => {
+const ShowScreen = ({ navigation }) => {
+  const { state } = React.useContext(BlogContext);
+  const blogPost = state.find(blogPost => (
+    blogPost.id === navigation.getParam('id')
+  ));
+
   return (
     <View>
       <Text>Show Screen</Text>
+      <Text>{blogPost.title} - {blogPost.id}</Text>
     </View>
   );
 };
